ef16ba92e0a474427c639fdc446433951e32940c
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2023-01-07 12:18:32 +0530

    Update RenderingTopic.js

--- src/components/RenderingTopic.js
+++ src/components/RenderingTopic.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const RenderingTopic = () => {
+  const history=useHistory();
   return (
     <div id="rendering-div">
       <h3>Rendering in React</h3>
-      <button id="rendering-back-button">
-        Back
-      </button>
+      
+        <button id="rendering-back-button" onClick={()=>history.push("/topics")}>Back</button>
+      
     </div>
   )
 }
